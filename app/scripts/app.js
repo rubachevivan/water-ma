d0d7e3dcf9a22ca16b0d7c1c881c05bc9484c46e
Rubachev Ivan <rubachevivan@icloud.com>
2016-01-07 18:21:25 +0300

    Final gulpfile added

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,4 +1,8 @@
 //water-ma main script.
 //application will be written in angular
 
-alert('hello world');
+//alert('hello world');
+for(var i = 0; i < 5; i++) {
+   console.log(3);
+}
+//alert('bye world');
--- gulpfile.js
+++ gulpfile.js
@@ -1,54 +1,88 @@
 'use strict';
-//including gulp
-var gulp = require('gulp');
 
-//include plug-ins
-var minifycss = require('gulp-minify-css'),
-    jshint = require('gulp-jshint'),
-    stylish = require('jshint-stylish'),
-    ngannotate = require('gulp-ng-annotate'),
-    uglify = require('gulp-uglify'),
-    usemin = require('gulp-usemin'),
-    imagemin = require('gulp-imagemin'),
-    rename = require('gulp-rename'),
-    concat = require('gulp-concat'),
-    notify = require('gulp-notify'),
-    cache = require('gulp-cache'),
-    changed = require('gulp-changed'),
-    rev = require('gulp-rev'),
-    browserSync = require('browser-sync'),
-    del = require('del');
+var gulp = require('gulp'),
+   minifycss = require('gulp-minify-css'),
+   autoprefixer = require('gulp-autoprefixer'),
+   jshint = require('gulp-jshint'),
+   stylish = require('jshint-stylish'),
+   uglify = require('gulp-uglify'),
+   usemin = require('gulp-usemin'),
+   imagemin = require('gulp-imagemin'),
+   rename = require('gulp-rename'),
+   concat = require('gulp-concat'),
+   notify = require('gulp-notify'),
+   cache = require('gulp-cache'),
+   changed = require('gulp-changed'),
+   rename = require('gulp-rename'),
+   browserSync = require('browser-sync'),
+   del = require('del');
 
-//jshintin all javascript files
+
 gulp.task('jshint', function() {
    return gulp.src('app/scripts/**/*.js')
       .pipe(jshint())
       .pipe(jshint.reporter(stylish));
 });
 
-// clean - delete all files in the dist folder
+// Clean
 gulp.task('clean', function() {
    return del(['dist']);
 });
 
+
+
 gulp.task('usemin', ['jshint'], function() {
    return gulp.src('./app/index.html')
       .pipe(usemin({
-         css: [minifycss(), rev()],
-         js: [ngannotate(), uglify(), rev()]
+         css: [autoprefixer(), minifycss(), rename({suffix: '.min'})],
+         js: [uglify(), rename({suffix: '.min'})]
       }))
       .pipe(gulp.dest('dist/'));
 });
 
-//images
+// Images
 gulp.task('imagemin', function() {
    return del(['dist/images']), gulp.src('app/images/**/*')
-    .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
-    .pipe(gulp.dest('dist/images'))
-    .pipe(notify({ message: 'Images task complete' }));
+      .pipe(cache(imagemin({
+         optimizationLevel: 3,
+         progressive: true,
+         interlaced: true
+      })))
+      .pipe(gulp.dest('dist/images'))
+      .pipe(notify({
+         message: 'Images task complete'
+      }));
 });
 
-// default task
+// Watch
+gulp.task('watch', ['browser-sync'], function() {
+   // Watch .js files
+   gulp.watch('{app/scripts/**/*.js,app/styles/**/*.css,app/**/*.html}', ['usemin']);
+   // Watch image files
+   gulp.watch('app/images/**/*', ['imagemin']);
+
+});
+
+gulp.task('browser-sync', ['default'], function() {
+   var files = [
+      'app/**/*.html',
+      'app/styles/**/*.css',
+      'app/images/**/*.png',
+      'app/scripts/**/*.js',
+      'dist/**/*'
+   ];
+
+   browserSync.init(files, {
+      server: {
+         baseDir: "dist",
+         index: "index.html"
+      }
+   });
+   // Watch any files in dist/, reload on change
+   gulp.watch(['dist/**']).on('change', browserSync.reload);
+});
+
+// Default task
 gulp.task('default', ['clean'], function() {
-   gulp.start('usemin', 'imagemin', 'copyfonts');
+   gulp.start('usemin', 'imagemin');
 });
